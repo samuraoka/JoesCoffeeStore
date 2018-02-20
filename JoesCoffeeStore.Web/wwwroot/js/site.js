4e98b341bd5e505ee7a899e04f0ee137d81d4248
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-02-20 14:58:15 +0900

    Added a JavaScript code

--- JoesCoffeeStore.Web/wwwroot/js/site.js
+++ JoesCoffeeStore.Web/wwwroot/js/site.js
@@ -1,1 +1,10 @@
 ﻿// Write your JavaScript code.
+
+// .ready()
+// api.jquery.com/ready/
+$(document).ready(function () {
+    var b = $("#addToCartButton");
+    b.on("click", function () {
+        window.alert("Your coffee was added to your cart.");
+    });
+});
